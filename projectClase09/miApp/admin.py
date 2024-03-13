2cf7f712270ed701aa17917ea423b6efa5b0e314
MartinAle90 <martinale90@gmail.com>
2024-03-13 17:08:23 -0300

    Al proyecto de Book se le agrego un super usuario y lo necesario para que se muestre el crud por administrador

--- projectClase09/miApp/admin.py
+++ projectClase09/miApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # ordenar por 'id', si se antepone el simbolo menos (-) orden descendente
+    # sin simbolo, es orden ascendente.
+    ordering = ['id']
+    # campos o columanas que se muestran en la grilla
+    # En list_display podemos agregar 'id' para mostrar en la grilla
+    list_display = ['nombre', 'autor', 'disponible']
+    # Estables los campos sobre los cuales vamos a realizar la busqueda
+    search_fields = ["nombre", "autor"]
+    
+
+admin.site.register(Book, BookAdmin)
--- projectClase09/miApp/models.py
+++ projectClase09/miApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
-    nombre=models.CharField(max_length=500)
-    autor=models.CharField(max_length=200)
+    nombre=models.CharField(max_length=100, verbose_name ="Titulo")
+    autor=models.CharField(max_length=100, blank=True)
     disponible=models.BooleanField(default=True)
     
     def __str__(self):
